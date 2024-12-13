9c0cad2ed57ad16cf9cb84f02025808b30f3c1e9
Anand Sudeep <anandsudeep5@gmail.com>
2024-12-13 16:30:53 +0530

    target client chat

--- index.js
+++ index.js
@@ -2,19 +2,53 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const { Server } = require("socket.io");
+const { callbackify } = require("util");
 
 const app = express();
 const server = http.createServer(app);
+
 app.use(express.static(path.resolve("./public")));
+
 const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile("./public/index.html");
 });
 
+app.get("/chat", (req, res) => {
+  res.sendFile("./public/chat.html");
+});
+
+let user = {};
+
 io.on("connection", (socket) => {
-  socket.on("user-message", (msg) => {
-    io.emit("s-msg", msg);
+  socket.on("login", (username) => {
+    user[username] = socket.id;
+    console.log(username + "logged in");
+  });
+
+  socket.on("sendMessage", (data) => {
+    const { sender, receiver, message } = data;
+
+    if (user[receiver]) {
+      if (user[receiver] === user[sender]) {
+        socket.emit("error", "you cant send message to yourself");
+      } else {
+        io.to(user[receiver]).emit("receiveMessage", { sender, message });
+      }
+    } else {
+      socket.emit("error", "User not online");
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (let username in user) {
+      if (user[username] === socket.id) {
+        delete user[username];
+        console.log(username + " disconnected");
+        break;
+      }
+    }
   });
 });
 
